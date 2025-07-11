fb558814416be719aa386825b4ba6653ce5074ab
shivank kunwar <shivankkunwar100@gmail.com>
2025-07-12 02:18:40 +0530

    minor fix

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import { JwtPayload } from "jsonwebtoken";
 
 export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.header("authorization");
@@ -8,10 +9,14 @@ export const verifyToken = (req: Request, res: Response, next: NextFunction) => 
   }
 
   const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.JWT_SECRET ?? "secret", (err, decoded) => {
-    if (err) return res.status(401).json({ error: "Unauthorized" });
-    (req as any).user = decoded;
-    (req as any).userId = (decoded as any).userId;
-    next();
-  });
+  jwt.verify(
+    token,
+    process.env.JWT_SECRET ?? "secret",
+    (err: Error | null, decoded: string | JwtPayload | undefined) => {
+      if (err) return res.status(401).json({ error: "Unauthorized" });
+      (req as any).user = decoded;
+      (req as any).userId = (decoded as any)?.userId;
+      next();
+    }
+  );
 }; 
